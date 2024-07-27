5478d6c9ac1b6d2dbe45c29c163fff23f0317307
Elvin Otieno <84793772+elvnski@users.noreply.github.com>
2024-07-27 13:27:46 +0300

    WIP LEETCODE #20
    :alien:

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,11 @@
 import LongestCommonPrefix.LongestCommonPrefixSolutions;
+import MergeTwoSortedLists.ListNode;
+import MergeTwoSortedLists.MySolution;
 import PalindromeNumber.PalindromeNumberSolutions;
 import RomanToInteger.RomanToIntegerSolutions;
 import TwoSum.BruteForce;
 import TwoSum.TwoPassHashTable;
 import TwoSum.TwoSumSolutions;
-import ValidParentheses.MySolution;
 import ValidParentheses.ValidParenthesesSolutions;
 
 import java.util.Arrays;
@@ -13,29 +14,32 @@ public class Main {
     public static void main(String[] args) {
 
 
-        //LEETCODE 1: TWO SUM
-        System.out.println("LEETCODE #1: TWO SUM");
-        TwoSumSolutions twoSumSolutions = new TwoSumSolutions();
-        twoSumSolutions.solve(new int[]{0,4,3,0}, 0);
+//        //LEETCODE 1: TWO SUM
+//        System.out.println("LEETCODE #1: TWO SUM");
+//        TwoSumSolutions twoSumSolutions = new TwoSumSolutions();
+//        twoSumSolutions.solve(new int[]{0,4,3,0}, 0);
+//
+//
+//        System.out.println("\nLEETCODE #9: PALINDROME NUMBER");
+//        PalindromeNumberSolutions palindromeNumberSolutions = new PalindromeNumberSolutions();
+//        palindromeNumberSolutions.solve(757);
+//
+//
+//        System.out.println("\nLEETCODE #13: ROMAN TO INTEGER");
+//        RomanToIntegerSolutions romanToIntegerSolutions = new RomanToIntegerSolutions();
+//        romanToIntegerSolutions.solve("MCMXCIV");
+//
+//
+//        System.out.println("\nLEETCODE #14: LONGEST COMMON PREFIX");
+//        LongestCommonPrefixSolutions longestCommonPrefixSolutions = new LongestCommonPrefixSolutions();
+//        longestCommonPrefixSolutions.solve(new String[]{"a", "aaa", "aba", "aaaaac"});
+//
+//
+//        System.out.println("\nLEETCODE #20: VALID PARENTHESES");
+//        (new ValidParenthesesSolutions()).solve("(){}}{");
 
 
-        System.out.println("\nLEETCODE #9: PALINDROME NUMBER");
-        PalindromeNumberSolutions palindromeNumberSolutions = new PalindromeNumberSolutions();
-        palindromeNumberSolutions.solve(757);
-
-
-        System.out.println("\nLEETCODE #13: ROMAN TO INTEGER");
-        RomanToIntegerSolutions romanToIntegerSolutions = new RomanToIntegerSolutions();
-        romanToIntegerSolutions.solve("MCMXCIV");
-
-
-        System.out.println("\nLEETCODE #14: LONGEST COMMON PREFIX");
-        LongestCommonPrefixSolutions longestCommonPrefixSolutions = new LongestCommonPrefixSolutions();
-        longestCommonPrefixSolutions.solve(new String[]{"a", "aaa", "aba", "aaaaac"});
-
-
-        System.out.println("\nLEETCODE #20: VALID PARENTHESES");
-        (new ValidParenthesesSolutions()).solve("(){}}{");
+        System.out.println((new MySolution()).mergeTwoLists(new ListNode(), new ListNode(0)));
 
     }
 }
