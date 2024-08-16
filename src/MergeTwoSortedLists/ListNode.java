69a96e7a8ff84322c739ee8b7dc4760cbaa08318
Elvin Otieno <84793772+elvnski@users.noreply.github.com>
2024-08-16 09:09:22 +0300

    Added toString method in ListNode
    :alien:

--- src/MergeTwoSortedLists/ListNode.java
+++ src/MergeTwoSortedLists/ListNode.java
@@ -2,20 +2,29 @@ package MergeTwoSortedLists;
 
 public class ListNode {
 
-     int val;
-     ListNode next;
+    int val;
+    ListNode next;
 
-     public ListNode() {}
+    public ListNode() {}
 
-     public ListNode(int val) {
+    public ListNode(int val) {
+        this.val = val;
+    }
 
-         this.val = val;
-     }
+    public ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
 
-     public ListNode(int val, ListNode next) {
-
-         this.val = val;
-         this.next = next;
-     }
-
- }
+    @Override
+    public String toString() {
+        StringBuilder result = new StringBuilder();
+        ListNode current = this;
+        while (current != null) {
+            result.append(current.val).append(" -> ");
+            current = current.next;
+        }
+        result.append("null");
+        return result.toString();
+    }
+}
